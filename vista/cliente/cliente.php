cb04f7868e798738f0158959fdb8619bdeed5dfa
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2024-11-11 06:38:33 -0500

    se anadio jQuery, Popper.js, Bootstrap JS

--- vista/cliente/cliente.php
+++ vista/cliente/cliente.php
@@ -25,5 +25,11 @@ if (session_status() == PHP_SESSION_NONE) {
         include './cliente-navbar.php';
         echo '<h1>Bienvenido cliente con id: '.$_SESSION['id_usuario'].'</h1>';
         ?>
+    
+        <!-- jQuery, Popper.js, Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     </body>
 </html>
