be7a4e38bac11d24397005ef2e980e301b3dfab5
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2024-11-11 06:07:09 -0500

    update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Resturante</title>
-        
-        <!-- Bootstrap CSS -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-        
-    </head>
-    <body>
-        
-        <?php
-        
-        header("Location: vista/pagina_principal/main.php");
-        exit();
-        
-        ?>
-    </body>
-</html>
+<?php
+header("Location: vista/pagina_principal/main.php");
+exit();
+?>
