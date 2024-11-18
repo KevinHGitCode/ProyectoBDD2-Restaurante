a7f184c313605095cc69958eca1a495d5c4ad087
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2024-11-17 20:57:39 -0500

    fixed

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-session_start();
-
-// Guarda el tipo de mensaje y texto en la sesión
-$_SESSION['mensaje'] = [
-    'exito' => true, // true para éxito, false para error
-    'texto' => 'La acción se realizó correctamente.'
-];
-
-// Redirige a la página principal
 header("Location: vista/pagina_principal/main.php");
 exit();
 ?>
